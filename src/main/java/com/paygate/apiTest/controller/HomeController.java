b58307248dd85749e49713b40ae0fa457b8a1084
cheolhee.jo <cheolhee.jo@INTPC037.PAYGATE.KR>
2020-12-28 09:17:13 +0900

    2020.12.28

---
+++ src/main/java/com/paygate/apiTest/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.paygate.apiTest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/getReceipt")
+	public String getReceipt() {
+		return "receipt";
+	}
+}
